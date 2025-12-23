e6e411024b6ffb5bb8bb461332eb4175f7dcf03b
Ajuzie Sinachi <Ajuziesinachi@gmail.com>
2025-12-23 11:13:22 +0100

    feat: Contracts - weth converter contract for converting compound weth to aave weth and vice versa.

---
+++ Contracts/src/WethConverter.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IWETH {
+    function deposit() external payable;
+    function withdraw(uint256 amount) external;
+    function transfer(address to, uint256 amount) external returns (bool);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+}
+
+contract WETHConverter {
+    IWETH public immutable wethAave;
+    IWETH public immutable wethCom;
+    
+    event WETHConverted(
+        address indexed user,
+        address indexed fromWETH,
+        address indexed toWETH,
+        uint256 amount
+    );
+    
+    constructor(address _wethAave, address _wethCom) {
+        require(_wethAave != address(0), "Invalid WETH wethAave address");
+        require(_wethCom != address(0), "Invalid WETH wethCom address");
+        require(_wethAave != _wethCom, "WETH addresses must be different");
+        
+        wethAave = IWETH(_wethAave);
+        wethCom = IWETH(_wethCom);
+    }
+    
+    function convertwethAaveTowethCom(uint256 amount) external {
+        _convert(wethAave, wethCom, amount);
+    }
+    
+    function convertwethComTowethAave(uint256 amount) external {
+        _convert(wethCom, wethAave, amount);
+    }
+    
+    function _convert(IWETH fromWETH, IWETH toWETH, uint256 amount) internal {
+        require(amount > 0, "Amount must be greater than 0");
+        
+        // // Transfer WETH from user to this contract
+        // require(
+        //     fromWETH.transferFrom(msg.sender, address(this), amount),
+        //     "Transfer failed"
+        // );
+        
+        // Withdraw ETH from the source WETH
+        fromWETH.withdraw(amount);
+        
+        // Deposit ETH to the target WETH
+        toWETH.deposit{value: amount}();
+        
+        // // Transfer the new WETH back to user
+        // require(
+        //     toWETH.transfer(msg.sender, amount),
+        //     "Transfer back failed"
+        // );
+        
+        emit WETHConverted(msg.sender, address(fromWETH), address(toWETH), amount);
+    }
+    
+    // Emergency function to recover any ETH stuck in contract
+    function emergencyWithdraw() external {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+    
+    // Required to receive ETH from WETH withdraw
+    receive() external payable {}
+}
